6dc1b3f943eb5b0ae9c9612eab5b2b44731f5ff4
Alexander Strasser <alexander.strasser@protonmail.com>
2025-09-15 14:56:33 +0200

    Update autogit.rb

--- autogit.rb
+++ autogit.rb
@@ -2,7 +2,7 @@ class Autogit < Formula
   desc "Automated git workflow tool"
   homepage "https://github.com/schwunder/autogit"
   url "https://github.com/schwunder/autogit/releases/download/v.1.0.1/autogit"
-  version "1.0.1"
+  version " v.1.0.1"
   sha256 "3f28f066226232b4b4fab1b5defb5c660d339ecbbbc39959ff99431b691724f6"
 
   def install
